ac467e25da6f82da594fbb5fd832ade8b40e7c90
pi shop <92638329+pishopofficial@users.noreply.github.com>
2023-07-16 03:33:54 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -10,7 +10,7 @@ const currentTempEl = document.getElementById('current-temp');
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-const API_KEY ='bf9203a34b73ecaf9fc96913f3de51fd';
+const API_KEY ='';
 
 setInterval(() => {
     const time = new Date();
